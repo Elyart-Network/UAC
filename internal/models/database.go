a56cf414bfd1eaeca767c90191b137fe849caa4d
Season Shi <seasonshi@elyart.dev>
2023-04-19 23:44:34 +0800

    UAC-20 fix(sql): unique key

--- internal/models/database.go
+++ internal/models/database.go
@@ -1,40 +1,40 @@
 package models
 
 type Clients struct {
-	ID       int    `json:"id" gorm:"primaryKey;autoIncrement;not null"`
-	Name     string `json:"name" gorm:"not null"`
-	Secret   string `json:"secret" gorm:"not null"`
+	ID       int    `json:"id" gorm:"primaryKey;autoIncrement;not null;unique"`
+	Name     string `json:"name" gorm:"not null;unique"`
+	Secret   string `json:"secret" gorm:"not null;unique"`
 	Type     string `json:"type" gorm:"not null"`
 	Provider int    `json:"provider"`
 	Addition string `json:"addition"`
 }
 
 type Providers struct {
-	ID   int    `json:"id" gorm:"primaryKey;autoIncrement;not null"`
-	Name string `json:"name" gorm:"not null"`
+	ID   int    `json:"id" gorm:"primaryKey;autoIncrement;not null;unique"`
+	Name string `json:"name" gorm:"not null;unique"`
 	Type string `json:"type" gorm:"not null"`
 	Data string `json:"data" gorm:"not null"`
 }
 
 type Credentials struct {
-	ID         int    `json:"id" gorm:"primaryKey;autoIncrement;not null"`
-	ClientID   int    `json:"client_id" gorm:"not null"`
-	Credential string `json:"credential" gorm:"not null"`
+	ID         int    `json:"id" gorm:"primaryKey;autoIncrement;not null;unique"`
+	ClientID   int    `json:"client_id" gorm:"not null;unique"`
+	Credential string `json:"credential" gorm:"not null;unique"`
 }
 
 type Users struct {
-	ID       int    `json:"id" gorm:"primaryKey;autoIncrement;not null"`
-	Username string `json:"username" gorm:"not null"`
-	Password string `json:"password" gorm:"not null"`
-	Email    string `json:"email" gorm:"not null"`
+	ID       int    `json:"id" gorm:"primaryKey;autoIncrement;not null;unique"`
+	Username string `json:"username" gorm:"not null;unique"`
+	Password string `json:"password" gorm:"not null;unique"`
+	Email    string `json:"email" gorm:"not null;unique"`
 	Addition string `json:"addition"`
 }
 
 type Tokens struct {
-	ID       int    `json:"id" gorm:"primaryKey;autoIncrement;not null"`
+	ID       int    `json:"id" gorm:"primaryKey;autoIncrement;not null;unique"`
 	UserID   int    `json:"user_id" gorm:"not null"`
 	ClientID int    `json:"client_id" gorm:"not null"`
-	Token    string `json:"token" gorm:"not null"`
+	Token    string `json:"token" gorm:"not null;unique"`
 	Expired  int64  `json:"expired" gorm:"not null"` // unix timestamp
 	Type     string `json:"type" gorm:"not null"`    // access or refresh
 	Addition string `json:"addition"`
